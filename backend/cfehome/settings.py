1aa9ed487840eee2615371f26f391b3320d1f7aa
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-09-23 14:03:57 +0800

    Added Products model and migrate database

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'api' #Register the app in the settings.py file.
+    'api',
+    'products',
 ]
 
 MIDDLEWARE = [
