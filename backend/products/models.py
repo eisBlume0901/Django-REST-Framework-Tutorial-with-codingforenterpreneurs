da6f58a737e38cd5f31f820a8f0db63b7f0e7d55
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-09-23 16:04:10 +0800

    Added str method so that product objects are readable by their title

--- backend/products/models.py
+++ backend/products/models.py
@@ -5,3 +5,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=0.0) # 12345.67
+    def __str__(self):
+        return self.title
