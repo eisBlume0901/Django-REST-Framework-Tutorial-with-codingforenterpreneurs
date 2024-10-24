c5c216f0b7487bdc9fc30aeb73101717cb492762
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-10-24 11:36:56 +0800

    Added sale_price field in ProductSerializer to show the sale price of each product.

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,7 +10,7 @@ from products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['title', 'content', 'price']
+        fields = ['title', 'content', 'price', 'sale_price']
 
     # The SerializerMethodField is used for custom fields that are not in the model.
     # The method name should be the same as the property name in the model.
